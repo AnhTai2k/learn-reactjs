b798d84795bfde7a71509686b41e41f9ec0f8595
Trinh Anh Tai <taitase140833@fpt.edu.vn>
2022-11-07 23:04:27 +0700

    update

--- src/SearchComponents/SearchBar.js
+++ src/SearchComponents/SearchBar.js
@@ -37,11 +37,11 @@ function SearchBar({placeholder, data}) {
                 ) : (
                     <CloseIcon id="clearBtn" onClick={clearInput}/>
                     )}             
-            </div>
+            </div>            
         </div>
-        {filteredData.length != 0 && (
+        {filteredData.length !== 0 && (
         <div className="dataResult">
-            {filteredData.slice(0, 15).map((value, key) => {
+            {filteredData.map((value, key) => {
                 return (
                 <a className="dataItem" href={value.link} target="_blank">
                     <p>
